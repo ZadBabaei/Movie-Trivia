305ee2a878b7ad03bf67f32fc51ebf1697380813
zad Babaei <zadbabaei@gmail.com>
2024-03-22 08:03:55 -0230

    adding the structure

--- App.js
+++ App.js
@@ -5,3 +5,15 @@ function nextHandeler() {
     count++
     console.log(reza);
 }
+
+
+
+
+for ( let correctAnswers = 0 ;correctAnswers < 10; correctAnswers++) {   
+    console.log('Wait starts');
+    setTimeout(() => {
+      console.log('Wait ends after 2 seconds');
+      correctAnswers++;
+      document.getElementById("correctAnswers").innerText = correctAnswers
+    }, 2000); // 2000 milliseconds = 2 seconds
+}
